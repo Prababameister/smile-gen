d6c7bcf7e50219760a2abb0074fd1b4d26b7a7c3
Prabhvir Babra <theprabab@gmail.com>
2023-04-09 10:35:57 -0700

    Remove build files

--- build/CMakeFiles/smile-gen-run.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for smile-gen-run.
-# This may be replaced when dependencies are built.
--- build/lib/foreign/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prabab/Programming/smile-gen/lib/foreign/glm
-# Build directory: /home/prabab/Programming/smile-gen/build/lib/foreign/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
